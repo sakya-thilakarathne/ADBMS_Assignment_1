be1b70db1e4f2ec451da8387690a50837ba22661
Sakya Thilakarathne <91802025+sakya-thilakarathne@users.noreply.github.com>
2023-09-05 22:39:53 +0530

    update

--- Microservices/Order/app.js
+++ Microservices/Order/app.js
@@ -1,11 +1,20 @@
 const express = require('express');
+const dbConnection = require('./dbconnection');
+const orderRoutes = require('./Route/OrderRoute');
 const app = express();
 
+const PORT = 3002;
 
-const PORT = 3002; 
+dbConnection.connectToDatabase();
+
+// Middleware to parse JSON requests
+app.use(express.json());
+
+// Use the order routes
+app.use('/order', orderRoutes);
 
 app.listen(PORT, () => {
-    console.log(`Order is running on port ${PORT}`);
+  console.log(`Order is running on port ${PORT}`);
 });
 
 module.exports = app;
